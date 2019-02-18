0735e3227ca6fb1809fd52ff361c72e323b34646
Vanderley Sousa da Silva Junior <vanderley_1998@hotmail.com.br>
2019-02-17 23:55:39 -0300

    remove config firebase

--- src/assets/js/database.js
+++ src/assets/js/database.js
@@ -5,14 +5,7 @@ import { error } from 'util';
 export default class Database {
 
     constructor(){
-        this.config = {
-            apiKey: "AIzaSyDS0OPAPWxXzPBsHCk4XkpQrsXcXYJFfH8",
-            authDomain: "hello-firebase-cdd76.firebaseapp.com",
-            databaseURL: "https://hello-firebase-cdd76.firebaseio.com",
-            projectId: "hello-firebase-cdd76",
-            storageBucket: "hello-firebase-cdd76.appspot.com",
-            messagingSenderId: "906217691288"
-          };
+        this.config = {};
           Firebase.initializeApp(this.config);
           this.db = Firebase.database()
     }
