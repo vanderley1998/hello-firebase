e188ab5672889dc37f29860761a9003fa05caa49
Vanderley Sousa <vanderley_1998@hotmail.com.br>
2019-02-20 16:14:03 -0300

    ikç

--- src/assets/js/firebase/connectionDB.js
+++ src/assets/js/firebase/connectionDB.js
@@ -8,14 +8,7 @@ import Auth from './auth'
 export default class ConnectionDB {
 
     constructor(){
-        this.config = {
-            apiKey: "AIzaSyDS0OPAPWxXzPBsHCk4XkpQrsXcXYJFfH8",
-            authDomain: "hello-firebase-cdd76.firebaseapp.com",
-            databaseURL: "https://hello-firebase-cdd76.firebaseio.com",
-            projectId: "hello-firebase-cdd76",
-            storageBucket: "hello-firebase-cdd76.appspot.com",
-            messagingSenderId: "906217691288"
-        }
+        this.config = {}
         Firebase.initializeApp(this.config)
         this.database = new Database(Firebase.database())
         this.auth = new Auth(Firebase.auth())
